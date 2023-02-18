42045a4bde59b5db650bb21f7a0050dc480e1cf9
pakdot05 <marckasayan0001@gmail.com>
2023-02-18 08:35:26 +0800

    first commit

---
+++ src/samplegit/SampleGit.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package samplegit;
+
+/**
+ *
+ * @author SCC COMPLAB
+ */
+public class SampleGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
